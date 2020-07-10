379b1bd607a25fc8727db9b6fc9569ee4d6fc77b
Nil SJ <niladri123@gmail.com>
2020-07-09 23:49:37 -0400

    Week 4 - NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        }
+        else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
