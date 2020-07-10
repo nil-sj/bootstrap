e2f8b4a71d0305a4007eb2a5063adace42d91c2c
Nil SJ <niladri123@gmail.com>
2020-07-10 13:55:48 -0400

    Bootstrap - End of Week 4

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,10 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
 });
